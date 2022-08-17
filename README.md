ab742ac6735373b246e26b9e6b769b571ea8f843
karlaevelize <k.evelize@gmail.com>
2022-08-17 15:06:16 +0200

    server 61

---
+++ README.md
@@ -0,0 +1,18 @@
+### Server/Client discussion
+### Packages
+### Express
+
+**Steps for installing packages**
+
+1. `npm init --y`
+2. Install packages `npm i [package-name]`
+
+**Steps for Express**
+
+1. Import express
+2. Start an express instance
+3. Define a port
+4. Start listening
+5. Run it with ``
+
+<img src="./image.png"/>
---
+++ index.js
@@ -0,0 +1,50 @@
+//import packages
+const express = require("express")
+
+//get data
+const data = require("./data.json")
+
+//express instance
+const app = express()
+
+//give a port/address
+const port = 4000
+
+//two arguments: path, function(request, response, next)
+app.get("/", (request, response) => {
+  response.send("Hello")
+})
+
+//message
+app.get("/message/:name", (request, response) => {
+  const name = request.params
+  const message = `Welcome, ${name}`
+  response.send(message)
+})
+
+//cat
+app.get("/cat", (request, response) => {
+  response.send("Cat")
+})
+
+//list of chars
+app.get("/characters", (request, response) => {
+  response.send(data)
+})
+
+//send a character by id
+app.get("/characters/:id", (request, response) => {
+  const id = request.params.id
+  const charById = data.find((char) => char.id === parseInt(id))
+  response.send(charById)
+})
+
+//send a character by id
+app.get("/chars/:blood", (request, response) => {
+  const blood = request.params.blood
+  const charByBlood = data.filter((char) => char.blood.toLowerCase() === blood.toLowerCase())
+  response.send(charByBlood)
+})
+
+//takes two arguments: path, function
+app.listen(port, () => console.log(`Listening on ${port}`))
